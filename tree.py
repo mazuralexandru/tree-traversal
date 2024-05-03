95bdf940d8b162a4d482bb677505512b319d1514
Alexandru Mazur <alexandru.mazur@code11.com>
2024-05-03 23:52:17 +0300

    Add GitHub action to run unit tests

--- tree.py
+++ tree.py
@@ -1,5 +1,10 @@
 from node import Node
 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
 
 class Tree:
     """ Tree class for binary tree """
@@ -62,27 +67,68 @@ class Tree:
             return self._find(data, node.right)
 
     def deleteTree(self):
-        # TODO 1
+        """ Method to clear the entire tree, removing all nodes.
+        
+        This sets the root to None, effectively deleting all references to the nodes.
+        """
         self.root = None
 
     def printTree(self):
-        # TODO 1
+        """ Print the entire tree in an inorder traversal.
+
+        This method serves as a wrapper to initiate the inorder traversal print starting from the root.
+        """
         if self.root is not None:
             self._printInorderTree(self.root)
 
     def _printInorderTree(self, node):
-        # TODO 1
+        """ Recursive method to print the tree's data in an inorder manner.
+
+        Args:
+            node (Node): The current node to process.
+
+        This method recursively visits the left child, prints the current node's data, then visits the right child.
+        """
         if node is not None:
             self._printInorderTree(node.left)
             print(str(node.data) + ' ')
             self._printInorderTree(node.right)
 
     def _printPreorderTree(self, node):
-        # TODO 2
-        pass
+        """ Recursive method to print the tree's data in a preorder manner. """
+        if node is not None:
+            print(str(node.data) + ' ', end='')
+            self._printPreorderTree(node.left)
+            self._printPreorderTree(node.right)
 
     def _printPostorderTree(self, node):
-        # TODO 2
-        pass
+        """ Recursive method to print the tree's data in a postorder manner. """
+        if node is not None:
+            self._printPostorderTree(node.left)
+            self._printPostorderTree(node.right)
+            print(str(node.data) + ' ', end='')
+
+import unittest
 
 
+class TestTreeFindMethod(unittest.TestCase):
+
+    def test_find_existing_data(self):
+        tree = Tree()
+        values = [20, 10, 30, 5, 15]
+        for value in values:
+            tree.add(value)
+        result = tree.find(15)
+        self.assertIsNotNone(result)
+        self.assertEqual(result.data, 15)
+
+    def test_find_non_existing_data(self):
+        tree = Tree()
+        values = [20, 10, 30, 5, 15]
+        for value in values:
+            tree.add(value)
+        result = tree.find(100)
+        self.assertIsNone(result)
+
+if __name__ == '__main__':
+    unittest.main()
